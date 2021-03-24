fca44b9cfe16e38ec750add069c6492dd2d6606d
teracy55 <literacy1945@gmail.com>
2021-03-24 21:08:11 +0900

    経歴詳細の見た目修正

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AboutComponent } from './about.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
     declarations: [AboutComponent],
-    imports: [CommonModule, MatTabsModule],
+    imports: [CommonModule, MatTabsModule, FlexLayoutModule],
 })
 export class AboutModule {}
