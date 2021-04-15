e5ec620d583892762aa7352d94efa775bf322ce3
teracy55 <literacy1945@gmail.com>
2021-04-15 17:38:20 +0900

    資格タブ修正

--- src/app/about/qualifications/qualifications.component.ts
+++ src/app/about/qualifications/qualifications.component.ts
@@ -5,4 +5,23 @@ import { Component } from '@angular/core';
     templateUrl: './qualifications.component.html',
     styleUrls: ['./qualifications.component.scss', '../shared/styles.scss'],
 })
-export class QualificationsComponent {}
+export class QualificationsComponent {
+    readonly qualifications = [
+        {
+            date: '2018/06/20',
+            name: '応用情報技術者試験',
+        },
+        {
+            date: '2015/02',
+            name: 'サーティファイC言語プログラミング能力認定試験1級',
+        },
+        {
+            date: '2011/08/31',
+            name: 'Androidアプリケーション技術者認定試験ベーシック',
+        },
+        {
+            date: '2011/08/05',
+            name: '基本情報技術者試験',
+        },
+    ];
+}
--- src/app/about/qualifications/qualifications.module.ts
+++ src/app/about/qualifications/qualifications.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { QualificationsComponent } from './qualifications.component';
 
 @NgModule({
     declarations: [QualificationsComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, FlexLayoutModule],
     exports: [QualificationsComponent],
 })
 export class QualificationsModule {}
