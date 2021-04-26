9f0b1d990230dbd1331c3d342099e2be56b2bfbd
teracy55 <literacy1945@gmail.com>
2021-04-26 19:14:19 +0900

    Productsページ改修

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -4,4 +4,17 @@ import { Component } from '@angular/core';
     templateUrl: './products.component.html',
     styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+    readonly products = [
+        {
+            name: 'パスワードマネージャー',
+            url: 'https://teracy55.github.io/password-manager/',
+            description: 'Google Drive上でパスワード情報を管理するシンプルなパスワードマネージャーです',
+        },
+        {
+            name: 'Image Downloader',
+            url: 'https://github.com/teracy55/img-downloader',
+            description: '指定のWebページ内の画像を一括ダウンロードするためのコマンドラインツールです',
+        },
+    ];
+}
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products.component';
 import { ProductsRoutingModule } from './products-routing.module';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
     declarations: [ProductsComponent],
-    imports: [CommonModule, ProductsRoutingModule],
+    imports: [CommonModule, ProductsRoutingModule, MatCardModule],
 })
 export class ProductsModule {}
