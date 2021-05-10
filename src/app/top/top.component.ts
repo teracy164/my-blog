4c755123b6fbe324268d64204765fefbe3a00dce
teracy55 <literacy1945@gmail.com>
2021-05-10 20:33:02 +0900

    TOP画面が画像読み込み後に表示されるように修正

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -7,16 +7,28 @@ import { AngularFireStorage } from '@angular/fire/storage';
     styleUrls: ['./top.component.scss'],
 })
 export class TopComponent implements OnInit {
+    /** 初期化中 */
+    isInitializing = true;
+    /** 画像読み込み中 */
+    isLoading = true;
     imgUrl: string;
 
     constructor(private storage: AngularFireStorage) {}
 
-    ngOnInit() {
-        this.storage
-            .ref('images/top.jpg')
-            .getDownloadURL()
-            .subscribe((image) => {
-                this.imgUrl = image;
-            });
+    async ngOnInit() {
+        await this.loadImage();
+
+        this.isInitializing = false;
+
+        // 一定時間経っても画像読み込みが終わらない場合は表示する
+        setTimeout(() => (this.isLoading = false), 3000);
+    }
+
+    private async loadImage() {
+        this.imgUrl = await this.storage.ref('images/top.jpg').getDownloadURL().toPromise();
+    }
+
+    onLoadedImage() {
+        this.isLoading = false;
     }
 }
