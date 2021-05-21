90478ebb719716e3b112fcb45222b8accc10859c
teracy55 <literacy1945@gmail.com>
2021-05-21 20:41:23 +0900

    Productsページの本サイトの説明を修正

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -5,15 +5,21 @@ import { Component } from '@angular/core';
     styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent {
+    test = '<strong>test</strong>';
     readonly products = [
         {
             name: '本サイト',
-            description: `本サイトはAngularにてスクラッチ開発しています。バックエンドにはFirebaseを使用しています`,
+            description:
+                '本サイトはAngularにてスクラッチ開発しています。<br>' +
+                'バックエンドにはFirebaseを使用しています<br>' +
+                '<br>' +
+                'なお、本サイトの構築についてはQiitaで解説しています<br>' +
+                '<a target="_blank" href="https://qiita.com/teracy55/items/bc35bb21f98d3d493f9f">https://qiita.com/teracy55/items/bc35bb21f98d3d493f9f</a>',
         },
         {
             name: 'パスワードマネージャー',
             url: 'https://teracy55.github.io/password-manager/',
-            description: 'Google Drive上でパスワード情報を管理するシンプルなパスワードマネージャーです。' + 'Angular ＋ GoogleAPIで作成。',
+            description: 'Google Drive上でパスワード情報を管理するシンプルなパスワードマネージャーです。<br>' + 'Angular ＋ GoogleAPIで作成。',
         },
         {
             name: 'Image Downloader',
