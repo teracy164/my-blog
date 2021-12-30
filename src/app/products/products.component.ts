742a4132f41f3e190a9c74b415cb4d71a4e4a0ad
teracy55 <literacy@gmail.com>
2021-12-30 20:21:26 +0900

    product概要修正

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -27,6 +27,7 @@ export class ProductsComponent {
             url: 'https://teracy-img-downloader.herokuapp.com/',
             description:
                 '指定のWebページ内の画像を一括ダウンロードするためWebツールです。<br>' +
+                'Nuxt.js + ElementUIで作成しています<br>' +
                 '※HEROKUの無料プランで動かしているため、サーバースリープ時は表示に1分ほど時間がかかることがあります',
         },
         {
@@ -38,7 +39,9 @@ export class ProductsComponent {
             name: 'KAIASTooo - システム開発サポートツール',
             url: 'https://kaisatooo.herokuapp.com/',
             description:
-                '※開発中<BR>システム開発全般（見積もり／設計／テスト／手順書等ドキュメント）の管理を行うためのシステムです。<br>' +
+                '※開発中<BR>' +
+                'システム開発全般（見積もり／設計／テスト／手順書等ドキュメント）の管理を行うためのシステムです。<br>' +
+                'Nuxt.js + ElementUIで作成しています<br>' +
                 '※HEROKUの無料プランで動かしているため、サーバースリープ時は表示に1分ほど時間がかかることがあります',
         },
     ];
